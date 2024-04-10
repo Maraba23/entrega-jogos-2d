d196e6079a003f54375b9338de7c6947d6584b6d
gustavolp1 <glpachecocsa@gmail.com>
2024-04-10 09:38:42 -0300

    prototype1

---
+++ Assets/Scripts/BluePlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePlayerController : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = 0f;
+        if (Input.GetKey(KeyCode.J) && Input.GetKey(KeyCode.L))
+        {
+            moveX = 0f;
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            moveX = -1f;
+        }
+        else if (Input.GetKey(KeyCode.L))
+        {
+            moveX = 1f;
+        }
+
+        rb.velocity = new Vector2(moveX * maxSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.I) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Box")
+        {
+            isGrounded = true;
+        }
+    }
+}
---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//minimum x = -8.375
+//maximum x = 8.375
+
+public class BoxController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
